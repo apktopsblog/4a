8771ac883e318e597075b4eb42f9fd42a613a805
apktopsblog <35693587+apktopsblog@users.noreply.github.com>
2023-07-17 03:24:14 +0330

    Update unis.sh

--- unis.sh
+++ unis.sh
@@ -2,54 +2,22 @@
 
 sleep 3
 
-service passwall stop
+cd /root/
 
-if [[ -f panel.ipk ]]
+echo "Unistalling ..."
 
-then 
+rm owo.sh
 
-  rm panel.ipk
+rm up.sh
 
-else 
+rm gigabit.sh
 
-  echo "Stage 2 Passed" 
+rm /etc/init.d/4a
 
-fi
-
-
-cd /tmp
-echo "Downloading latest version of Xray-core ..."
-wget -q https://amir3.space/panel.ipk
-
-if [[ -f panel.ipk ]]
-
-then 
-
-  echo " Download Successful !"
-
-else 
-
-  echo " ERROR : Download Unsuccessful "
-
-fi
-
-echo "INSTALLING XRAY ..."
-opkg install panel.ipk -d ram
-
-
-if [[ -f panel.ipk ]]
-
-then 
-
-  rm panel.ipk
-
-else 
-
-  echo "Stage 1 Passed" 
-
-fi
-
-service passwall restart
 cd /root/
 
 echo "Done !"
+
+sleep 5
+
+rm unis.sh
