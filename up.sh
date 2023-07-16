9bc91737b903fa766abb5cd8c24293a896290953
apktopsblog <35693587+apktopsblog@users.noreply.github.com>
2023-07-17 03:25:31 +0330

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
+echo "Running as root..."
 
 sleep 3
 
-cd /root/
+clear
 
-echo "Unistalling ..."
+echo "Updating Please Wait..."
+
+service passwall stop
+
+cd /root/
 
 rm owo.sh
 
-rm up.sh
+wget https://raw.githubusercontent.com/apktopsblog/4a/main/owo.sh
 
-rm amirhossein.sh
+chmod 777 owo.sh
 
-rm /etc/init.d/amir
+cd /etc/init.d/
+
+rm amir
+
+wget https://raw.githubusercontent.com/apktopsblog/4a/main/4a
+
+chmod +x /etc/init.d/4a
+
+/etc/init.d/4a enable
 
 cd /root/
 
-echo "Done !"
 
-sleep 5
+/etc/init.d/4a start
 
-rm unis.sh
+echo -e "${GREEN} Update Complated ! ${ENDCOLOR}"
