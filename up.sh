3d82bbd0fac576d83d450152bdbbca06d9d266cc
apktopsblog <35693587+apktopsblog@users.noreply.github.com>
2023-07-17 03:15:31 +0330

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sleep 3
+
+cd /root/
+
+echo "Unistalling ..."
+
+rm owo.sh
+
+rm up.sh
+
+rm amirhossein.sh
+
+rm /etc/init.d/amir
+
+cd /root/
+
+echo "Done !"
+
+sleep 5
+
+rm unis.sh
